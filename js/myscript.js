64a5e1b6af6e763a1d1174ea24c9dbe5ed0a9602
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-05 14:51:05 +0200

    start to image in js

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,14 @@ createApp({
     data() {
         return {
             message: 'Hello Vue!',
-
+            images : [
+                "01.webp",
+                "02.webp",
+                "03.webp",
+                "04.webp",
+                "05.webp"
+            ],
+            activeIndex : 0,
         }
     },
     
